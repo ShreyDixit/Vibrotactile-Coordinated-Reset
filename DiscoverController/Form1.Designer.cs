d9660f1e07d0004066363d5223e5771a015a0bc0
shrey <shrey@SHREY>
2023-09-26 16:26:05 +0200

    Add project files.

---
+++ DiscoverController/Form1.Designer.cs
@@ -0,0 +1,245 @@
+﻿namespace DiscoverController
+{
+    partial class DiscoverControllerForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.PulseTactor1Button = new System.Windows.Forms.Button();
+            this.ConnectButton = new System.Windows.Forms.Button();
+            this.DiscoverButton = new System.Windows.Forms.Button();
+            this.ComPortLabel = new System.Windows.Forms.Label();
+            this.ComPortComboBox = new System.Windows.Forms.ComboBox();
+            this.ConsoleOutputRichTextBox = new System.Windows.Forms.RichTextBox();
+            this.SimulationStartButton = new System.Windows.Forms.Button();
+            this.GainTrackBar = new System.Windows.Forms.TrackBar();
+            this.MirrorHands = new System.Windows.Forms.CheckBox();
+            this.RandomizGain = new System.Windows.Forms.CheckBox();
+            this.GainValueLabel = new System.Windows.Forms.Label();
+            this.GainValue = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.vCRDuration = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.GainTrackBar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // PulseTactor1Button
+            // 
+            this.PulseTactor1Button.Enabled = false;
+            this.PulseTactor1Button.Location = new System.Drawing.Point(22, 214);
+            this.PulseTactor1Button.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.PulseTactor1Button.Name = "PulseTactor1Button";
+            this.PulseTactor1Button.Size = new System.Drawing.Size(386, 68);
+            this.PulseTactor1Button.TabIndex = 9;
+            this.PulseTactor1Button.Text = "Pulse Tactor 1";
+            this.PulseTactor1Button.UseVisualStyleBackColor = true;
+            this.PulseTactor1Button.Click += new System.EventHandler(this.PulseTactor1Button_Click);
+            // 
+            // ConnectButton
+            // 
+            this.ConnectButton.Enabled = false;
+            this.ConnectButton.Location = new System.Drawing.Point(22, 137);
+            this.ConnectButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.ConnectButton.Name = "ConnectButton";
+            this.ConnectButton.Size = new System.Drawing.Size(386, 68);
+            this.ConnectButton.TabIndex = 8;
+            this.ConnectButton.Text = "Connect";
+            this.ConnectButton.UseVisualStyleBackColor = true;
+            this.ConnectButton.Click += new System.EventHandler(this.ConnectButton_Click);
+            // 
+            // DiscoverButton
+            // 
+            this.DiscoverButton.Location = new System.Drawing.Point(22, 60);
+            this.DiscoverButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.DiscoverButton.Name = "DiscoverButton";
+            this.DiscoverButton.Size = new System.Drawing.Size(386, 68);
+            this.DiscoverButton.TabIndex = 7;
+            this.DiscoverButton.Text = "Discover";
+            this.DiscoverButton.UseVisualStyleBackColor = true;
+            this.DiscoverButton.Click += new System.EventHandler(this.DiscoverButton_Click);
+            // 
+            // ComPortLabel
+            // 
+            this.ComPortLabel.AutoSize = true;
+            this.ComPortLabel.Location = new System.Drawing.Point(18, 23);
+            this.ComPortLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.ComPortLabel.Name = "ComPortLabel";
+            this.ComPortLabel.Size = new System.Drawing.Size(75, 20);
+            this.ComPortLabel.TabIndex = 6;
+            this.ComPortLabel.Text = "Com Port";
+            // 
+            // ComPortComboBox
+            // 
+            this.ComPortComboBox.FormattingEnabled = true;
+            this.ComPortComboBox.Location = new System.Drawing.Point(102, 18);
+            this.ComPortComboBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.ComPortComboBox.Name = "ComPortComboBox";
+            this.ComPortComboBox.Size = new System.Drawing.Size(304, 28);
+            this.ComPortComboBox.TabIndex = 5;
+            // 
+            // ConsoleOutputRichTextBox
+            // 
+            this.ConsoleOutputRichTextBox.Location = new System.Drawing.Point(22, 651);
+            this.ConsoleOutputRichTextBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.ConsoleOutputRichTextBox.Name = "ConsoleOutputRichTextBox";
+            this.ConsoleOutputRichTextBox.ReadOnly = true;
+            this.ConsoleOutputRichTextBox.Size = new System.Drawing.Size(386, 182);
+            this.ConsoleOutputRichTextBox.TabIndex = 10;
+            this.ConsoleOutputRichTextBox.Text = "";
+            // 
+            // SimulationStartButton
+            // 
+            this.SimulationStartButton.Enabled = false;
+            this.SimulationStartButton.Location = new System.Drawing.Point(20, 361);
+            this.SimulationStartButton.Name = "SimulationStartButton";
+            this.SimulationStartButton.Size = new System.Drawing.Size(386, 67);
+            this.SimulationStartButton.TabIndex = 11;
+            this.SimulationStartButton.Text = "Start";
+            this.SimulationStartButton.UseVisualStyleBackColor = true;
+            this.SimulationStartButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // GainTrackBar
+            // 
+            this.GainTrackBar.Enabled = false;
+            this.GainTrackBar.Location = new System.Drawing.Point(20, 523);
+            this.GainTrackBar.Maximum = 255;
+            this.GainTrackBar.Minimum = 1;
+            this.GainTrackBar.Name = "GainTrackBar";
+            this.GainTrackBar.Size = new System.Drawing.Size(386, 69);
+            this.GainTrackBar.TabIndex = 12;
+            this.GainTrackBar.Value = 150;
+            this.GainTrackBar.Scroll += new System.EventHandler(this.GainTrackBar_Scroll);
+            // 
+            // MirrorHands
+            // 
+            this.MirrorHands.Appearance = System.Windows.Forms.Appearance.Button;
+            this.MirrorHands.AutoSize = true;
+            this.MirrorHands.Enabled = false;
+            this.MirrorHands.Location = new System.Drawing.Point(20, 454);
+            this.MirrorHands.MaximumSize = new System.Drawing.Size(455, 455);
+            this.MirrorHands.Name = "MirrorHands";
+            this.MirrorHands.Size = new System.Drawing.Size(110, 30);
+            this.MirrorHands.TabIndex = 13;
+            this.MirrorHands.Text = "Mirror Hands";
+            this.MirrorHands.UseVisualStyleBackColor = true;
+            // 
+            // RandomizGain
+            // 
+            this.RandomizGain.Appearance = System.Windows.Forms.Appearance.Button;
+            this.RandomizGain.AutoSize = true;
+            this.RandomizGain.Enabled = false;
+            this.RandomizGain.Location = new System.Drawing.Point(268, 454);
+            this.RandomizGain.Name = "RandomizGain";
+            this.RandomizGain.Size = new System.Drawing.Size(138, 30);
+            this.RandomizGain.TabIndex = 14;
+            this.RandomizGain.Text = "Randomize Gain";
+            this.RandomizGain.UseVisualStyleBackColor = true;
+            this.RandomizGain.CheckedChanged += new System.EventHandler(this.RandomizGain_CheckedChanged);
+            // 
+            // GainValueLabel
+            // 
+            this.GainValueLabel.AutoSize = true;
+            this.GainValueLabel.Location = new System.Drawing.Point(70, 595);
+            this.GainValueLabel.Name = "GainValueLabel";
+            this.GainValueLabel.Size = new System.Drawing.Size(88, 20);
+            this.GainValueLabel.TabIndex = 15;
+            this.GainValueLabel.Text = "Gain Value";
+            // 
+            // GainValue
+            // 
+            this.GainValue.Enabled = false;
+            this.GainValue.Location = new System.Drawing.Point(205, 592);
+            this.GainValue.Name = "GainValue";
+            this.GainValue.ReadOnly = true;
+            this.GainValue.Size = new System.Drawing.Size(181, 26);
+            this.GainValue.TabIndex = 16;
+            this.GainValue.Text = "150";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 313);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(215, 20);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "Simulation Duration (in mins.)";
+            // 
+            // vCRDuration
+            // 
+            this.vCRDuration.Enabled = false;
+            this.vCRDuration.Location = new System.Drawing.Point(268, 310);
+            this.vCRDuration.Name = "vCRDuration";
+            this.vCRDuration.Size = new System.Drawing.Size(138, 26);
+            this.vCRDuration.TabIndex = 18;
+            this.vCRDuration.Text = "1";
+            // 
+            // DiscoverControllerForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(423, 866);
+            this.Controls.Add(this.vCRDuration);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.GainValue);
+            this.Controls.Add(this.GainValueLabel);
+            this.Controls.Add(this.RandomizGain);
+            this.Controls.Add(this.MirrorHands);
+            this.Controls.Add(this.GainTrackBar);
+            this.Controls.Add(this.SimulationStartButton);
+            this.Controls.Add(this.ConsoleOutputRichTextBox);
+            this.Controls.Add(this.PulseTactor1Button);
+            this.Controls.Add(this.ConnectButton);
+            this.Controls.Add(this.DiscoverButton);
+            this.Controls.Add(this.ComPortLabel);
+            this.Controls.Add(this.ComPortComboBox);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "DiscoverControllerForm";
+            this.Text = "Discover Controller Tutorial";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.DiscoverControllerForm_FormClosed);
+            ((System.ComponentModel.ISupportInitialize)(this.GainTrackBar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button PulseTactor1Button;
+        private System.Windows.Forms.Button ConnectButton;
+        private System.Windows.Forms.Button DiscoverButton;
+        private System.Windows.Forms.Label ComPortLabel;
+        private System.Windows.Forms.ComboBox ComPortComboBox;
+        private System.Windows.Forms.RichTextBox ConsoleOutputRichTextBox;
+        private System.Windows.Forms.Button SimulationStartButton;
+        private System.Windows.Forms.TrackBar GainTrackBar;
+        private System.Windows.Forms.CheckBox MirrorHands;
+        private System.Windows.Forms.CheckBox RandomizGain;
+        private System.Windows.Forms.Label GainValueLabel;
+        private System.Windows.Forms.TextBox GainValue;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox vCRDuration;
+    }
+}
+
---
+++ DiscoverController/Form1.cs
@@ -0,0 +1,239 @@
+﻿/* ---------------------------------------------------------------------------
+** The software supplied herewith by Engineering Acoustics, Inc.
+** (the Company) for its Tactor Development Kit is intended and
+** supplied to you, the Company's customer, for use solely and
+** exclusively on Engineering Acoustics, Inc. products. The
+** software is owned by the Company and/or its supplier, and is
+** protected under applicable copyright laws. All rights are reserved.
+** Any use in violation of the foregoing restrictions may subject the
+** user to criminal sanctions under applicable laws, as well as to
+** civil liability for the breach of the terms and conditions of this
+** license.
+**
+** THIS SOFTWARE IS PROVIDED IN AN AS IS CONDITION. NO WARRANTIES,
+** WHETHER EXPRESS, IMPLIED OR STATUTORY, INCLUDING, BUT NOT LIMITED
+** TO, IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+** PARTICULAR PURPOSE APPLY TO THIS SOFTWARE. THE COMPANY SHALL NOT,
+** IN ANY CIRCUMSTANCES, BE LIABLE FOR SPECIAL, INCIDENTAL OR
+** CONSEQUENTIAL DAMAGES, FOR ANY REASON WHATSOEVER.
+**
+**   Copyright 2015(c) Engineering Acoustics Inc. All rights reserved.   *
+** -------------------------------------------------------------------------*/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using System.Linq;
+using System.Reflection;
+
+//-------------------------------------------------------------------------//
+//TDK Windows C# Discover Controller Tutorial
+//This project show how the discovery system work. When the user presses 
+//the discover button it calls discover on the tdk.
+//The combobox is then populated with the found devices.
+//The user can select wich device to connect to in the combo box and press
+//the connect button to connect to it.x
+//The user is able to send a pulse command to the board by pressing the 
+//pulse tactor 1 button
+//-------------------------------------------------------------------------//
+
+namespace DiscoverController
+{
+	public partial class DiscoverControllerForm : Form
+	{
+		private int ConnectedBoardID = -1;
+
+		public DiscoverControllerForm()
+		{
+			InitializeComponent();
+			//To initialize the TDKInterface we need to call InitializeTI before we use any
+			//of its functionality
+            WriteMessageToGUIConsole("InitializeTI\n");
+			CheckTDKErrors(Tdk.TdkInterface.InitializeTI());
+		}
+  
+		private void DiscoverButton_Click(object sender, EventArgs e)
+		{
+			WriteMessageToGUIConsole("Discover Started...\n");
+			//Discovers all serial tactor devices and returns the amount found
+			int ret = Tdk.TdkInterface.Discover((int)Tdk.TdkDefines.DeviceTypes.Serial);
+			if (ret > 0)
+			{
+				WriteMessageToGUIConsole("Discover Found:\n");
+				//populate combo box with discovered names
+				for (int i = 0; i < ret; i++)
+				{
+					//Gets the discovered device name at the index i
+					System.IntPtr discoveredNamePTR =  Tdk.TdkInterface.GetDiscoveredDeviceName(i);
+					if (discoveredNamePTR != null)
+					{
+						string sComName = Marshal.PtrToStringAnsi(discoveredNamePTR);
+						WriteMessageToGUIConsole(sComName + "\n");
+						ComPortComboBox.Items.Add(sComName);
+					}
+					else
+						WriteMessageToGUIConsole(Tdk.TdkDefines.GetLastEAIErrorString());
+				}
+				ComPortComboBox.SelectedIndex = 0;
+				DiscoverButton.Enabled = false;
+				ConnectButton.Enabled = true;
+			}
+			else
+			{
+				WriteMessageToGUIConsole("Discover Failed:\n");
+				WriteMessageToGUIConsole(Tdk.TdkDefines.GetLastEAIErrorString());
+			}
+		}
+
+		private void ConnectButton_Click(object sender, EventArgs e)
+		{
+			string selectedComPort = ComPortComboBox.SelectedItem.ToString();
+			WriteMessageToGUIConsole("\nConnecting to com port " + selectedComPort + "\n");
+			//Connect connects to the tactor controller via serial with the given name
+			//we should be hooking up a response callback but for simplicity of the 
+			//tutorial we wont be. Reference the ResponseCallback tutorial for more information
+			int ret = Tdk.TdkInterface.Connect(selectedComPort,
+											   (int)Tdk.TdkDefines.DeviceTypes.Serial,
+												System.IntPtr.Zero);
+			if (ret >= 0)
+			{
+				ConnectedBoardID = ret;
+				DiscoverButton.Enabled = false;
+				ConnectButton.Enabled = false;
+				PulseTactor1Button.Enabled = true;
+				SimulationStartButton.Enabled = true;
+				GainTrackBar.Enabled = true;
+				MirrorHands.Enabled = true;
+				RandomizGain.Enabled = true;
+				vCRDuration.Enabled = true;
+            }
+			else
+			{
+				WriteMessageToGUIConsole(Tdk.TdkDefines.GetLastEAIErrorString());
+			}
+		}
+
+		private void PulseTactor1Button_Click(object sender, EventArgs e)
+		{
+			WriteMessageToGUIConsole("Pulse tactor 1\n");
+            //This sends a command to the tactor controller to pulse tactor 1 for 250 milliseconds
+            CheckTDKErrors(Tdk.TdkInterface.Pulse(ConnectedBoardID, 1, 1000, 0));
+		}
+
+		private void WriteMessageToGUIConsole(string msg)
+		{
+			ConsoleOutputRichTextBox.AppendText(msg);
+		}
+
+		private void DiscoverControllerForm_FormClosed(object sender, FormClosedEventArgs e)
+		{
+			//closes up the connection to the tactor device with ConnectedBoardID
+			CheckTDKErrors(Tdk.TdkInterface.Close(ConnectedBoardID));
+			//cleans up everyting associated witht the TActionManager. Unloads any TActions loaded
+			CheckTDKErrors(Tdk.TdkInterface.ShutdownTI());
+		}
+		private void CheckTDKErrors(int ret)
+		{
+			//if a tdk method returns less then zero then we should display the last error
+			//in the tdk interface
+			if (ret < 0)
+			{
+				//the GetLastEAIErrorString returns a string that represents the last error code
+				//recorded in the tdk interface.
+				WriteMessageToGUIConsole(Tdk.TdkDefines.GetLastEAIErrorString());
+			}
+		}
+
+        private async void startButton_Click(object sender, EventArgs e)
+        {
+            // Simulation parameters
+            PulseTactor1Button.Enabled = false;
+            SimulationStartButton.Enabled = false;
+            GainTrackBar.Enabled = false;
+            MirrorHands.Enabled = false;
+            RandomizGain.Enabled = false;
+            vCRDuration.Enabled = false;
+            int Total_Simulation_Duration = (int)(decimal.Parse(vCRDuration.Text) * 60 * 1000);
+
+            CheckTDKErrors(Tdk.TdkInterface.ChangeGain(ConnectedBoardID, 0, GainTrackBar.Value, 0));
+
+            Task task1 = startFingerVibrationSimulation(Total_Simulation_Duration, RandomizGain.Checked);
+            await Task.WhenAll(task1);
+
+            PulseTactor1Button.Enabled = true;
+            SimulationStartButton.Enabled = true;
+            GainTrackBar.Enabled = true;
+            MirrorHands.Enabled = true;
+            RandomizGain.Enabled = true;
+            vCRDuration.Enabled = true;
+        }
+
+		private async Task startFingerVibrationSimulation(int Total_Simulation_Duration, bool randomizedGain = false, bool rightHand = false, int randomSeed = 42)
+		{
+            const int vCR_Cycle_Duration = 667; // ms
+            const int vCR_Duration = 100; // ms
+            const int vCR_Frequency = 2500; // Hz
+			const int num_fingers = 4;
+            const int vCR_Cycle_Duration_Single_Finger = vCR_Cycle_Duration / num_fingers;
+			const int inter_stimulus_interval = vCR_Cycle_Duration_Single_Finger - vCR_Duration;
+			const int jitter = (int)(inter_stimulus_interval * 0.235);
+            int Number_of_vCR_Cycles = Total_Simulation_Duration / vCR_Cycle_Duration;
+
+
+            Random rand = new Random(randomSeed);
+            int[] fingers = { 1, 2, 3, 4};
+
+            if (rightHand)
+			{
+				for (int i = 0; i < num_fingers; i++)
+					fingers[i] += 4;
+            }
+
+            CheckTDKErrors(Tdk.TdkInterface.ChangeFreq(ConnectedBoardID, 0, vCR_Frequency, 0));
+
+			if (randomizedGain)
+                foreach (int finger in fingers)
+                    CheckTDKErrors(Tdk.TdkInterface.ChangeGain(ConnectedBoardID, finger, rand.Next(1, 175), 0));
+
+            int current_vCR_Burst_Start = 0;
+			int next_vCR_Burst_Start = 0;
+
+            for (int vCR_Cycle = 0; vCR_Cycle < Number_of_vCR_Cycles; vCR_Cycle++)
+            {
+				if (vCR_Cycle % 5 < 3)
+				{
+                    fingers = fingers.OrderBy(x => rand.Next()).ToArray();
+                    for (int finger_idx = 0; finger_idx < num_fingers; finger_idx++)
+					{
+						current_vCR_Burst_Start = next_vCR_Burst_Start;
+                        CheckTDKErrors(Tdk.TdkInterface.Pulse(ConnectedBoardID, fingers[finger_idx], vCR_Duration, 0));
+						next_vCR_Burst_Start = rand.Next(-jitter, jitter);
+                        await Task.Delay(vCR_Cycle_Duration_Single_Finger + next_vCR_Burst_Start - current_vCR_Burst_Start);
+                    }
+                }
+				else
+				{
+                    await Task.Delay(vCR_Cycle_Duration);
+                }
+            }
+        }
+
+        private void GainTrackBar_Scroll(object sender, EventArgs e)
+        {
+			GainValue.Text = GainTrackBar.Value.ToString();
+        }
+
+        private void RandomizGain_CheckedChanged(object sender, EventArgs e)
+        {
+			if (RandomizGain.Checked)
+                GainTrackBar.Enabled = false;
+			else 
+				GainTrackBar.Enabled = true;
+        }
+    }
+}
